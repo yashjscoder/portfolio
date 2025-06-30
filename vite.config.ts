a5fa919181520ce981746c51a928be44230931d7
yashjscoder <148244687+yashjscoder@users.noreply.github.com>
2025-07-01 00:08:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace 'your-repo-name' with your actual GitHub repo name
-const repoName = "/your-repo-name/";
-
 export default defineConfig(({ mode }) => ({
-  base: repoName,  // <-- Add this line
+  base: "/portfolio/",  // Set this to your GitHub repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -23,4 +19,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
